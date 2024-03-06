64cbecd604b01a99ae63b257d399f26b4f169c6d
Siamak Barzegar <Siamak.Barzegar@cube.global>
2024-03-06 11:43:59 +1100

    updating comments in handler.py

--- src/handler.py
+++ src/handler.py
@@ -1,5 +1,3 @@
-""" Example handler file. """
-
 import runpod
 from lorax import AsyncClient, Client
 from loguru import logger
@@ -10,23 +8,19 @@ import os
 os.environ['HF_HUB_OFFLINE'] = 1
 
 
-# If your handler runs inference on a model, load the model here.
-# You will want models to be loaded into memory before starting serverless.
-JOBS = set()
-
 client_async = AsyncClient("http://127.0.0.1:8000")
 client = Client("http://127.0.0.1:8000")
 
-# Wait for the hugging face TGI worker to start running.
+# Wait for the LoRAX worker to start running.
 while True:
     try:
         x = client.generate("This is just for loading test", max_new_tokens=1).generated_text
-        print("Successfully cold booted the hugging face text generation inference server!")
+        print("Successfully cold booted the LoRAX server!")
         # Break from the while loop
         break
 
     except Exception as e:
-        print("The hugging face text generation inference server is still cold booting...")
+        print("The Lorax server is still cold booting...")
         time.sleep(5)
 
 
@@ -46,9 +40,7 @@ async def handler(job: dict):
     do_sample = parameters.get('do_sample')
     seed = parameters.get('seed')
 
-    # Add job to the set.
 
-    # Streaming case
     resp = await client_async.generate(prompt, max_new_tokens=max_new_tokens, temperature=temperature, top_p=top_p,
                            seed=seed,
                            do_sample=do_sample)
